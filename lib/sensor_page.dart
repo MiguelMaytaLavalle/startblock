cc9552f07927c90f6b6f5cb539b2f7d359396df9
Ricardo Mayta Lavalle <ricardo.mayta@hotmail.com>
2021-12-22 21:25:34 +0100

    Initial commit

---
+++ lib/sensor_page.dart
@@ -0,0 +1,182 @@
+import 'dart:async';
+import 'dart:convert' show utf8;
+
+import 'package:flutter/material.dart';
+import 'package:flutter_blue/flutter_blue.dart';
+import 'package:oscilloscope/oscilloscope.dart';
+
+class SensorPage extends StatefulWidget {
+  const SensorPage({Key? key, required this.device}) : super(key: key);
+  final BluetoothDevice device;
+
+  @override
+  _SensorPageState createState() => _SensorPageState();
+}
+
+class _SensorPageState extends State<SensorPage> {
+  final String SERVICE_UUID = "6e400002-b5a3-f393-e0a9-e50e24dcca9e";
+  final String CHARACTERISTIC_UUID = "6e400003-b5a3-f393-e0a9-e50e24dcca9e";
+  late bool isReady;
+  late Stream<List<int>> stream;
+  List<double> traceDust = [];
+
+  @override
+  void initState() {
+    super.initState();
+    isReady = false;
+    connectToDevice();
+  }
+
+  connectToDevice() async {
+    if (widget.device == null) {
+      _Pop();
+      return;
+    }
+
+    Timer(const Duration(seconds: 15), () {
+      if (!isReady) {
+        disconnectFromDevice();
+        _Pop();
+      }
+    });
+
+    await widget.device.connect();
+    discoverServices();
+  }
+
+  disconnectFromDevice() {
+    if (widget.device == null) {
+      _Pop();
+      return;
+    }
+
+    widget.device.disconnect();
+  }
+
+  discoverServices() async {
+    if (widget.device == null) {
+      _Pop();
+      return;
+    }
+
+    List<BluetoothService> services = await widget.device.discoverServices();
+    for (var service in services) {
+      if (service.uuid.toString() == SERVICE_UUID) {
+        for (var characteristic in service.characteristics) {
+          if (characteristic.uuid.toString() == CHARACTERISTIC_UUID) {
+            characteristic.setNotifyValue(!characteristic.isNotifying);
+            stream = characteristic.value;
+
+            setState(() {
+              isReady = true;
+            });
+          }
+        }
+      }
+    }
+
+    if (!isReady) {
+      _Pop();
+    }
+  }
+
+  Future<bool> _onWillPop() {
+    return showDialog(
+        context: context,
+        builder: (BuildContext context) =>
+        AlertDialog(
+          title: const Text('Are you sure?'),
+          content: const Text('Do you want to disconnect device and go back?'),
+          actions: <Widget>[
+            TextButton(
+                onPressed: () => Navigator.of(context).pop(false),
+                child: const Text('No')),
+            TextButton(
+                onPressed: () {
+                  disconnectFromDevice();
+                  Navigator.of(context).pop(true);
+                },
+                child: const Text('Yes')),
+          ],
+        )
+    ).then((value) => value ?? false);
+  }
+
+  _Pop() {
+    Navigator.of(context).pop(true);
+  }
+
+  String _dataParser(List<int> dataFromDevice) {
+    return utf8.decode(dataFromDevice);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Oscilloscope oscilloscope = Oscilloscope(
+      showYAxis: true,
+      //TODO deprecated padding change to margin
+      padding: 0.0,
+      backgroundColor: Colors.black,
+      traceColor: Colors.white,
+      yAxisMax: 3000.0,
+      yAxisMin: 0.0,
+      dataSet: traceDust,
+    );
+
+    return WillPopScope(
+      onWillPop: _onWillPop,
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('Sensor'),
+        ),
+        body: Container(
+            child: !isReady
+                ? const Center(
+              child: Text(
+                "Waiting...",
+                style: TextStyle(fontSize: 24, color: Colors.red),
+              ),
+            )
+                : StreamBuilder<List<int>>(
+                  stream: stream,
+                  builder: (BuildContext context,
+                      AsyncSnapshot<List<int>> snapshot) {
+                    if (snapshot.hasError) return Text('Error: ${snapshot.error}');
+
+                    if (snapshot.connectionState ==
+                        ConnectionState.active) {
+                      var currentValue = _dataParser(snapshot.data!);
+                      traceDust.add(double.tryParse(currentValue) ?? 0);
+
+                      return Center(
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: <Widget>[
+                              Expanded(
+                                flex: 1,
+                                child: Column(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: <Widget>[
+                                      const Text('Current value from Sensor',
+                                          style: TextStyle(fontSize: 14)),
+                                      Text('${currentValue}',
+                                          style: const TextStyle(
+                                              fontWeight: FontWeight.bold,
+                                              fontSize: 24))
+                                    ]),
+                              ),
+                              Expanded(
+                                flex: 1,
+                                child: oscilloscope,
+                              )
+                            ],
+                          ));
+                    } else {
+                      return const Text('Check the stream');
+                    }
+                  },
+                )),
+      ),
+    );
+  }
+}
