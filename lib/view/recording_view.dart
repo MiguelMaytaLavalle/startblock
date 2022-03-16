620aacfacc08e2ad1c8a54755366be64ac2c2368
Ricardo Mayta Lavalle <ricardo.mayta@hotmail.com>
2022-03-16 18:35:51 +0100

    updated sensor_page.dart

--- lib/view/recording_view.dart
+++ lib/view/recording_view.dart
@@ -1,8 +1,9 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:startblock/view_model/sensor_page_view_model.dart';
-//import 'package:camera/camera.dart';
+import 'package:camera/camera.dart';
 import '../helper/BLEController.dart';
+import 'package:gallery_saver/gallery_saver.dart';
 
 class RecordingScreen extends StatefulWidget {
   @override
@@ -13,10 +14,44 @@ class _RecordingState extends State<RecordingScreen> {
   var sensorPageVM = SensorPageViewModel();
   BLEController bleController = BLEController();
 
+  bool _isLoading = true;
+  bool _isRecording = false;
+  late CameraController _cameraController;
+
   @override
   void initState() {
     super.initState();
+    _initCamera();
   }
+
+  _initCamera() async {
+    final cameras = await availableCameras();
+    final front = cameras.firstWhere((camera) => camera.lensDirection == CameraLensDirection.back);
+    _cameraController = CameraController(front, ResolutionPreset.max);
+    await _cameraController.initialize();
+    setState(() => _isLoading = false);
+    //setState(() => sensorPageVM.setIsLoading(false));
+  }
+
+  _recordVideo() async {
+    if (_isRecording) {
+      final file = await _cameraController.stopVideoRecording();
+      setState(() => _isRecording = false);
+      await GallerySaver.saveVideo(file.path);
+/*      final route = MaterialPageRoute(
+        fullscreenDialog: true,
+        builder: (_) => VideoPage(filePath: file.path),
+      );*/
+      //Navigator.push(context, route);
+    } else {
+      await _cameraController.prepareForVideoRecording();
+      await _cameraController.startVideoRecording();
+      setState(() => _isRecording = true);
+    }
+  }
+
+
+
   Future<bool> _onWillPop() {
     return showDialog(
         context: context,
@@ -41,102 +76,133 @@ class _RecordingState extends State<RecordingScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return WillPopScope( onWillPop: _onWillPop,
-        child:
-        Scaffold(
-          appBar: AppBar(
-            /*title:Text('${bleController.targetDevice?.name}'),*/
-          ),
-          /*Column(
-          children: [
-            SizedBox(
-                height: 500,
-                child:  StreamBuilder<BluetoothDeviceState>(
-                  stream: targetDevice?.state,
-                  builder: (BuildContext context, snapshot) {
-                    if (snapshot.hasError) return Text('Error: ${snapshot.error}');
-                    if (snapshot.connectionState == ConnectionState.active) {
-                      return SafeArea(
-                        child: Scaffold(
-                          body: SfCartesianChart(
-                            //crosshairBehavior: _crosshairBehavior,
-                            legend: Legend(isVisible: true),
-                            //zoomPanBehavior: _zoomPanBehavior,
-                            series: _getUpdateSeries(),
-                            primaryXAxis: NumericAxis(
-                                interactiveTooltip: const InteractiveTooltip(
-                                  enable: true,
-                                ),
-                                majorGridLines: const MajorGridLines(width: 0),
-                                edgeLabelPlacement: EdgeLabelPlacement.shift,
-                                interval: 1000, //1000ms between two timestamps equals a second
-                                title: AxisTitle(text: 'Time [S]')
-                            ),
+    return WillPopScope(
+      onWillPop: _onWillPop,
+      child: _isLoading ? Container(
+      //child: sensorPageVM.getIsLoading() ? Container(
+        color: Colors.white,
+        child: const Center(
+          child: CircularProgressIndicator(),
 
-                            primaryYAxis: NumericAxis(
-                                minimum: 0,
-                                //maximum: 800,
-                                interactiveTooltip: const InteractiveTooltip(
-                                  enable: true,
-                                ),
-                                axisLine: const AxisLine(width: 0),
-                                majorTickLines: const MajorTickLines(size: 0),
-                                title: AxisTitle(text: 'Force [N]')
-                            ),
-                          ),
-                        ),
-                      );
-                    } else { return const Text('Check the stream'); }
-                  },
-                )
-            ),
-            Wrap(
-              direction: Axis.vertical,
-              children: const <Widget>[
-                Material(
-                  //margin:const EdgeInsets.all(10),
-                    child: Text('Rate of force (RFD): 0'
-                    )
-                ),
-                Material(
-                  //margin:const EdgeInsets.all(10),
-                    child: Text('Time to peak (TTP): 0'
-                    )
-                ),
-                Material(
-                  //margin:const EdgeInsets.all(10),
-                    child: Text('Force impulse: 0'
-                    )
-                ),
-                Material(
-                  //margin:const EdgeInsets.all(10),
-                    child: Text('Peak force: 0'
-                    )
-                ),
-              ],
+        ),
+      ):Center(
+        child: Stack(
+          alignment: Alignment.bottomCenter,
+          children: [
+            CameraPreview(_cameraController),
+            Padding(
+              padding: const EdgeInsets.all(25),
+              child: FloatingActionButton(
+                backgroundColor: Colors.red,
+                child: Icon(_isRecording ? Icons.stop : Icons.circle),
+                onPressed: () => _recordVideo(),
+                //onPressed: () => initGo(),
+              ),
             ),
           ],
-        ),*/
-          floatingActionButton:Wrap(
-            direction: Axis.horizontal,
-            children: <Widget>[
-              Container(
-                  margin:const EdgeInsets.all(10),
-                  child: ElevatedButton(
-                    onPressed: bleController.isNotStarted ? bleController.sendKrille: null,
-                    child: const Text('Krille'),
-                  )
-              ),
-              Container(
-                  margin:const EdgeInsets.all(10),
-                  child: ElevatedButton(
-                    onPressed: bleController.isNotStarted ? bleController.initGo: null,
-                    child: const Text('START'),
-                  )
-              ),
-            ],
-          ),
-        )
+        ),
+      ),
     );
+
+
+    // return WillPopScope(
+    //     onWillPop: _onWillPop,
+    //     child:
+    //     Scaffold(
+    //       appBar: AppBar(
+    //         /*title:Text('${bleController.targetDevice?.name}'),*/
+    //       ),
+    //       /*Column(
+    //       children: [
+    //         SizedBox(
+    //             height: 500,
+    //             child:  StreamBuilder<BluetoothDeviceState>(
+    //               stream: targetDevice?.state,
+    //               builder: (BuildContext context, snapshot) {
+    //                 if (snapshot.hasError) return Text('Error: ${snapshot.error}');
+    //                 if (snapshot.connectionState == ConnectionState.active) {
+    //                   return SafeArea(
+    //                     child: Scaffold(
+    //                       body: SfCartesianChart(
+    //                         //crosshairBehavior: _crosshairBehavior,
+    //                         legend: Legend(isVisible: true),
+    //                         //zoomPanBehavior: _zoomPanBehavior,
+    //                         series: _getUpdateSeries(),
+    //                         primaryXAxis: NumericAxis(
+    //                             interactiveTooltip: const InteractiveTooltip(
+    //                               enable: true,
+    //                             ),
+    //                             majorGridLines: const MajorGridLines(width: 0),
+    //                             edgeLabelPlacement: EdgeLabelPlacement.shift,
+    //                             interval: 1000, //1000ms between two timestamps equals a second
+    //                             title: AxisTitle(text: 'Time [S]')
+    //                         ),
+    //
+    //                         primaryYAxis: NumericAxis(
+    //                             minimum: 0,
+    //                             //maximum: 800,
+    //                             interactiveTooltip: const InteractiveTooltip(
+    //                               enable: true,
+    //                             ),
+    //                             axisLine: const AxisLine(width: 0),
+    //                             majorTickLines: const MajorTickLines(size: 0),
+    //                             title: AxisTitle(text: 'Force [N]')
+    //                         ),
+    //                       ),
+    //                     ),
+    //                   );
+    //                 } else { return const Text('Check the stream'); }
+    //               },
+    //             )
+    //         ),
+    //         Wrap(
+    //           direction: Axis.vertical,
+    //           children: const <Widget>[
+    //             Material(
+    //               //margin:const EdgeInsets.all(10),
+    //                 child: Text('Rate of force (RFD): 0'
+    //                 )
+    //             ),
+    //             Material(
+    //               //margin:const EdgeInsets.all(10),
+    //                 child: Text('Time to peak (TTP): 0'
+    //                 )
+    //             ),
+    //             Material(
+    //               //margin:const EdgeInsets.all(10),
+    //                 child: Text('Force impulse: 0'
+    //                 )
+    //             ),
+    //             Material(
+    //               //margin:const EdgeInsets.all(10),
+    //                 child: Text('Peak force: 0'
+    //                 )
+    //             ),
+    //           ],
+    //         ),
+    //       ],
+    //     ),*/
+    //       floatingActionButton:Wrap(
+    //         direction: Axis.horizontal,
+    //         children: <Widget>[
+    //           Container(
+    //               margin:const EdgeInsets.all(10),
+    //               child: ElevatedButton(
+    //                 onPressed: bleController.isNotStarted ? bleController.sendKrille: null,
+    //                 child: const Text('Krille'),
+    //               )
+    //           ),
+    //           Container(
+    //               margin:const EdgeInsets.all(10),
+    //               child: ElevatedButton(
+    //                 onPressed: bleController.isNotStarted ? bleController.initGo: null,
+    //                 child: const Text('START'),
+    //               )
+    //           ),
+    //         ],
+    //       ),
+    //     )
+    // );
   }
+
 }
